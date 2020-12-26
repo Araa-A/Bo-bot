40ecf32171041a2bb10b80e9a80d8dba3ea25a4e
0hAshes <br3adforall@gmail.com>
2020-12-26 23:45:49 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.DISCORD_BOT_SECRET;
+
+client.on('ready', () => {
+	console.log("Bobo's minion reporting for duty");
+	console.log(client.user.username);
+});
+
+client.login(token);
