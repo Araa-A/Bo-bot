005e22078e16c0fe59592100ead752acc890f5ba
0hAshes <Br3adforall@gmail.com>
2020-12-27 00:19:26 +0000

    worker update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.DISCORD_BOT_SECRET;
+//const token = process.env.DISCORD_BOT_SECRET;
 
 client.on('ready', () => {
 	console.log("Bobo's minion reporting for duty");
 	console.log(client.user.username);
 });
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
